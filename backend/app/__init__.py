376a45398d1f1924fa3746f9411df38bf2909a47
Sampath Lakkaraju <lakkaraju.sampath@outlook.com>
2025-04-19 20:53:16 -0700

    adding moc data fetch and adding CORS on bakend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from .routes import api
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"/api/*": {"origins": '*'}})
     app.register_blueprint(api)
     return app
